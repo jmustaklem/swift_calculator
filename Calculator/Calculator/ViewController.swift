634e582f02babbb6c36a2171fc76771fb5e5ddda
jmustaklem <jimustaklem@gmail.com>
2014-07-19 13:57:43 -0400

    Hooking up buttons

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var answerField: UILabel;
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func buttonTapped(AnyObject) {
-        println("Button Tapped!");
+    @IBAction func buttonTapped(theButton: UIButton) {
+        println(theButton.titleLabel.text);
     }
-
-
+    
+    @IBAction func plusTapped(theButton: UIButton) {
+        
+    }
+    
+    @IBAction func subtractTapped(theButton: UIButton) {
+        
+    }
 }
 
